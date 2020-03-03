7fe541b210ff1e03d7b076ee02f8a30a0e1d8dd8
tbalmeida <tbalmeida@gmail.com>
2020-03-03 05:25:19 +0000

    exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store1.employees.create(first_name: "Khurram", last_name: "", hourly_rate: 30)
+
+puts "What's the name of the new store?"
+store_name = gets.chomp
+@new_store = Store.create(name: store_name)
+errors = @new_store.errors.messages
+errors.each{ |message| puts message }
