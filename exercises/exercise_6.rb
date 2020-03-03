d8add5d8532b00422e51c898e672d1c39a039f91
tbalmeida <tbalmeida@gmail.com>
2020-03-03 01:33:36 +0000

    Exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -12,12 +12,10 @@ puts "----------"
 @store1 = Store.find_by(id: 1)
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "Jackyl", last_name: "Smith", hourly_rate: 30)
+@store1.employees.create(first_name: "Anna", last_name: "Islainova", hourly_rate: 80)
 
 @store2 = Store.find_by(id: 2)
 @store2.employees.create(first_name: "Hyde", last_name: "Black", hourly_rate: 47)
-@store2.employees.create(first_name: "Hyde", last_name: "Black", hourly_rate: 47)
+@store2.employees.create(first_name: "Hyde", last_name: "Black", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Plata", hourly_rate: 90)
 
-@store3 = Store.find_by(id: 3)
-@store3.employees.create(first_name: "Anna", last_name: "Islainova", hourly_rate: 47)
-@store3.employees.create(first_name: "John", last_name: "Plata", hourly_rate: 47)
-@store3.employees.create(first_name: "Stephen", last_name: "King", hourly_rate: 47)
